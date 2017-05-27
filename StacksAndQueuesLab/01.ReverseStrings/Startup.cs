c1d65fc9e7b188313b345fdc05250362c3879642
mgerin <marin_gerin@abv.bg>
2017-05-28 00:37:36 +0300

    Stack and Queues - Lab

---
+++ StacksAndQueuesLab/01.ReverseStrings/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ReverseStrings
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                stack.Push(ch);
+            }
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
