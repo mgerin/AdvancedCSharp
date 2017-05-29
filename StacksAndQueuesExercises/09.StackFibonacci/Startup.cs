551b5f7134d30c792b71ebb9a9cc330c31434ae7
mgerin <marin_gerin@abv.bg>
2017-05-29 16:30:32 +0300

    New things added

---
+++ StacksAndQueuesExercises/09.StackFibonacci/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.StackFibonacci
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stack = new Stack<long>();
+
+            stack.Push(0);
+            stack.Push(1);
+            for (int i = 3; i <= 70; i++)
+            {
+                var firstNumber = stack.Pop();
+                var secondNumber = stack.Peek();
+                stack.Push(firstNumber);
+                stack.Push(firstNumber + secondNumber);
+            }
+            for (int i = 1; i < 70 - n; i++)
+            {
+                stack.Pop();
+            }
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
---
+++ StacksAndQueuesExercises/10.SimpleTextEditor/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10.SimpleTextEditor
+{
+    class Startup
+    {
+       
+
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stack = new Stack<string>();
+            stack.Push(String.Empty);
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                var cmd = int.Parse(input[0]);
+
+                switch (cmd)
+                {
+                    case 1:
+                        var text = input[1];
+                        var update = stack.Peek() + text;
+                        stack.Push(update);
+                        break;
+                    case 2:
+                        var count = int.Parse(input[1]);
+                        var updateNew = stack.Peek();
+                        update = updateNew.Remove(updateNew.Length - count);
+                        stack.Push(update);
+                        break;
+                    case 3:
+                        var index = int.Parse(input[1]);
+                        var lastUpdate = stack.Peek();
+                        Console.WriteLine(lastUpdate[index - 1]);
+                        break;
+                    case 4:
+                        stack.Pop();
+                        break;
+                }
+            }
+        }
+    }
+}
