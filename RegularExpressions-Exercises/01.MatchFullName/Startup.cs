b133b29cf20fb69f1d90a5414e74dea9f782bf96
mgerin <marin_gerin@abv.bg>
2017-06-14 17:10:21 +0300

    Regex Exercises

---
+++ RegularExpressions-Exercises/01.MatchFullName/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.MatchFullName
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Startup
+    {
+        static void Main()
+        {
+            string input = String.Empty;
+            Regex regex = new Regex("\\b([A-Z][a-z]+) ([A-Z][a-z]+)\\b");
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+
+                Console.WriteLine(regex.Match(input));
+            }
+        }
+    }
+}
---
+++ RegularExpressions-Exercises/02.MatchPhoneNumber/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.MatchPhoneNumber
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Startup
+    {
+        static void Main()
+        {
+            const string pattern = "\\+359( |-)2\\1([0-9]{3})\\1([0-9]{4})\\b";
+            Regex regex = new Regex(pattern);
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                foreach (Match match in regex.Matches(input))
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    }
+}
