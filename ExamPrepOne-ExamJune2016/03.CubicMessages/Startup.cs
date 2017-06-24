7e7bd31c0160dd75872e2c57e75339c986790c23
Marin Gerin <marin_gerin@abv.bg>
2017-06-24 20:43:03 +0300

    New Things

--- ExamPrepOne-ExamJune2016/03.CubicMessages/Startup.cs
+++ ExamPrepOne-ExamJune2016/03.CubicMessages/Startup.cs
@@ -1,15 +1,53 @@
 ﻿namespace _03.CubicMessages
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
+    using System.Text.RegularExpressions;
 
     class Startup
     {
         static void Main()
         {
+            string pattern = "(^\\d+)([a-zA-Z]+)([^a-zA-Z]*$)";
+            string inputMessage;
+            while ((inputMessage = Console.ReadLine()) != "Over!")
+            {
+                var messageLength = int.Parse(Console.ReadLine());
+                var match = Regex.Match(inputMessage, pattern);
+
+                if (match.Success)
+                {
+                    var prefix = match.Groups[1].Value;
+                    var message = match.Groups[2].Value;
+                    var ending = string.Empty;
+                    if (match.Groups[3].Value != "")
+                    {
+                        ending = match.Groups[3].Value;
+                    }
+
+                    if (message.Length != messageLength)
+                    {
+                        continue;
+                    }
+
+                    var indexes = Regex.Replace(prefix + ending, @"\D*", String.Empty);
+                    var sb = new StringBuilder();
+                    foreach (var index in indexes)
+                    {
+                        var ind = int.Parse(index.ToString());
+                        if (ind >= 0 && ind < messageLength)
+                        {
+                            sb.Append(message[ind]);
+                        }
+                        else
+                        {
+                            sb.Append(" ");
+                        }
+                    }
+
+                    Console.WriteLine($"{message} == {sb}");
+                }
+            }
 
         }
     }
