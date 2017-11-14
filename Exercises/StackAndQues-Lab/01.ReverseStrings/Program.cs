e875e8173cc741a093eee4c0b0dd211d383cf055
mgerin <marin_gerin@abv.bg>
2017-11-14 17:11:00 +0200

    Exercise Stack and Queues

---
+++ Exercises/StackAndQues-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+
+            foreach (char ch in input)
+            {
+                stack.Push(ch);
+            }
+
+            while (stack.Count != 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Exercises/StackAndQues-Lab/02.SimpleCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02.SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            var arguments = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            var stack = new Stack<string>(arguments.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNumber = int.Parse(stack.Pop());
+                var op = stack.Pop();
+                var secondNumber = int.Parse(stack.Pop());
+
+                if (op == "+")
+                {
+                    stack.Push((firstNumber + secondNumber).ToString());
+                }
+                else
+                {
+                    stack.Push((firstNumber - secondNumber).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
